c8208179e97d1baf3a68a629494de7e8e5903d0e
freddypv <freddy@qburst.com>
2017-05-05 22:03:40 +0530

    created sender section

--- mailparser.js
+++ mailparser.js
@@ -1,5 +1,6 @@
 
 (function () {
+    $('h2').hide();
     $("#form_button").click(function () {
         $.ajax({
             url: "process.php",
@@ -8,14 +9,31 @@
             data: {'value': $('#email-header-data').val()},
             success: function (data)
             {
-                $('#dataTable tbody').html('');
+                $('#dataTable tbody,#dataTable1 tbody,#dataTable2 tbody').html('');
+                $('h2').hide();
                 $.each(data, function (i, val) {
-                    val = val.replace("&", "&amp;");
-                    val = val.replace("<", "&lt;");
-                    val = val.replace(">", "&gt;");
-                    $('#dataTable tbody').append('<tr><td class="key-section">' + i + '</td><td> ' + val + '</td></tr>')
-                    console.log(i, '--', val);
+
+                    if (i !== 'details') {
+                        val = val.replace("&", "&amp;");
+                        val = val.replace("<", "&lt;");
+                        val = val.replace(">", "&gt;");
+                        $('#dataTable tbody').append('<tr><td class="key-section">' + i + '</td><td> ' + val + '</td></tr>')
+                    } else {
+
+                        $.each(val, function (j, k) {
+                            $.each(k[0], function (m, n) {
+                                $('#dataTable1 tbody').append('<tr><td class="key-section">' + m + '</td><td> ' + n + '</td></tr>')
+                            });
+                            $.each(k[1], function (m, n) {
+                                $('#dataTable2 tbody').append('<tr><td class="key-section">' + m + '</td><td> ' + n + '</td></tr>')
+                            });
+                            $('#dataTable1 tbody').append('<tr class="empty"><td class=""></td><td> </td></tr>')
+                            $('#dataTable2 tbody').append('<tr class="empty"><td class=""></td><td></td></tr>')
+                        });
+                    }
+
                 });
+                $('h2').show();
             },
             error: function (jqXHR, textStatus, errorThrown)
             {
